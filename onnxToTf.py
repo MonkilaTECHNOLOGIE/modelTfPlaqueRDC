42f87b298133702377fcbe066553f90653db836e
Joe Monkila <jnkiwa25@gmail.com>
2024-09-05 09:24:41 +0100

    onnx to tf

---
+++ onnxToTf.py
@@ -0,0 +1,11 @@
+import onnx
+from onnx_tf.backend import prepare
+
+# Charger le modèle ONNX
+onnx_model = onnx.load("best_model/best.onnx")
+
+# Convertir en modèle TensorFlow
+tf_rep = prepare(onnx_model)
+
+# Sauvegarder le modèle TensorFlow
+tf_rep.export_graph("model")
